f55cf2d2b3dbdde83cc5201909995598113be65f
nikhilucodesoft <nikhil.singla@ucodesoft.com>
2024-12-30 11:07:01 +0000

    statemic code

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\CustomApiController;
-use Illuminate\Support\Facades\Route;
-use Statamic\Facades\Entry;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,13 +8,3 @@ use Statamic\Facades\Entry;
 | Here is where you can register web routes for your application.
 |
 */
-
-// Example route that returns all entries from the 'pages' collection
-Route::get('/api/posts', function () {
-    dd('dd');
-    // Fetch all entries from the 'pages' collection
-    $entries = Entry::query()->where('collection', 'pages')->get();
-
-    // Return the entries as a JSON response
-    return response()->json($entries);
-});
